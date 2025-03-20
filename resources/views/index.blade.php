7cfe2e461556ec7eb58ded3191d41aea2568489e
Shalom <shalomhettiarachchi888@gmail.com>
2025-03-20 14:41:15 +0530

    gauge update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -17,5 +17,72 @@
             <p class="L-p"><span class="live-indicator"></span>Live</p>
         </div>
     </div>
+    <div id="chartDiv" style="max-width: 740px;height: 330px;margin: 0px auto"></div>
+                
+
+                <div class="populated-section">
+                    <p class="title-mini">Most Populated Divisions</p>
+                    <p class="txt-1">Colombo 04</p>
+                    <p class="txt-1">Colombo 07</p>
+                    <p class="txt-1">Colombo 10</p>
+                </div>
+                    
+                </div>
+            
+        
+            <script>
+                // Update timestamp dynamically
+                function updateTime() {
+                    const now = new Date();
+                    const timeString = now.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+                    document.querySelector('.txt-2').innerHTML = `Air quality index (AQI) in Colombo ${timeString}, ${now.toLocaleDateString()}`;
+                }
+                updateTime();
+                setInterval(updateTime, 60000); 
+              
+        
+                var chart = JSC.chart('chartDiv', { 
+                    debug: false, 
+                    legend_visible: false, 
+                    defaultTooltip_enabled: false, 
+                    xAxis_spacingPercentage: 0.4, 
+                    yAxis: [{ 
+                        id: 'ax1', 
+                        defaultTick: { padding: 10, enabled: false }, 
+                        customTicks: [0, 100, 200, 300, 400, 500], 
+                        line: { width: 10, color: 'smartPalette:pal1' }, 
+                        scale_range: [0, 500] 
+                    }], 
+                    defaultSeries: { 
+                        type: 'gauge column roundcaps', 
+                        shape: { 
+                            label: { 
+                                text: '%max', 
+                                align: 'center', 
+                                verticalAlign: 'middle', 
+                                style_fontSize: 28 
+                            } 
+                        } 
+                    }, 
+                    series: [{ 
+                        type: 'column roundcaps', 
+                        name: 'AQI', 
+                        yAxis: 'ax1', 
+                        palette: { 
+                            id: 'pal1', 
+                            pointValue: '%yValue', 
+                            ranges: [ 
+                                { value: 0, color: '#21D683' },  
+                                { value: 100, color: '#77E6B4' },
+                                { value: 200, color: '#FFD221' },
+                                { value: 300, color: '#FF9B21' },
+                                { value: 400, color: '#FF5353' }, 
+                                { value: 500, color: '#D60000' }  
+                            ] 
+                        }, 
+                        points: [['AQI', 250]] 
+                    }] 
+                }); 
+              </script>
 
 </x-layout>
