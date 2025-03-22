b037d1e3d45319244fea77d6ec56e9a636100fe2
Shalom <shalomhettiarachchi888@gmail.com>
2025-03-22 17:02:31 +0530

    Register pg

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,7 @@ Route::get('/dashboard', function () {
 Route::get('/login', function () {
     return view('login');
 })->name('login');
+
+Route::get('/register', function () {
+    return view('register');
+})->name('register');
